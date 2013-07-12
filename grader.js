5d96481909ad4d85c21f4e4663f98430ab388bfd
Joel Kotarski <kotarski@gmail.com>
2013-07-12 08:40:00 +0000

    URL support for grader

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var crypto = require('crypto');
+var urlFile = '';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +37,25 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertUrlValid = function(url) {
+    var urlStr = url.toString();
+    urlFile = 'f' + crypto.randomBytes(4).readUInt32LE(0) + '.html';
+    rest.get(urlStr).on('complete', function(result, response) {
+        if (result instanceof Error) {
+            console.log("%s is a URL causes following result: %s. Exiting.", url, result);
+            process.exit(1);
+        } else {
+//            console.log("Should write " + urlFile);
+            fs.writeFile(urlFile, result, function (err) { 
+                if (err) throw err;
+                program.file = urlFile;
+                completion();
+            });
+        }
+    });
+    return urlStr;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -61,14 +83,29 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var completion = function() {
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    var urlFileExists = urlFile && fs.existsSync(urlFile);
+    if (urlFileExists) {
+        fs.unlink(urlFile, function(err) { 
+            if (err) throw err;
+        });
+    }
+    console.log(outJson);
+
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('--url <url>', 'URL to download', clone(assertUrlValid)) 
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+//    console.log('main:url=' + program.url + ';urlFile=' + urlFile + ';file=' + program.file);
+    if (!program.url && program.file && program.checks) {
+        completion();
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
